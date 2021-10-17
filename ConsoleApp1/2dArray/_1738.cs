c127d314068b8abe439feb3a47f0c99cb52e91ce
Khanh Gia Nguyen <ngiakhanh96@gmail.com>
2021-10-18 00:41:52 +0700

    Add 2d array prefix XOR

---
+++ ConsoleApp1/2dArray/_1738.cs
@@ -0,0 +1,65 @@
+﻿namespace ConsoleApp1._2dArray
+{
+    public class _1738
+    {
+        public int KthLargestValue(int[][] matrix, int k)
+        {
+            //var fakePriorityQueue = new List<int>();
+            //var priorityQueue = new PriorityQueue<int, int>();
+            var count = 0;
+            var temp = new int[matrix.Length * matrix[0].Length];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    matrix[i][j] =
+                        (i > 0 ? matrix[i - 1][j] : 0) ^
+                        (j > 0 ? matrix[i][j - 1] : 0) ^
+                        (i > 0 && j > 0 ? matrix[i - 1][j - 1] : 0) ^
+                        matrix[i][j];
+                    //if (priorityQueue.Count < k)
+                    //{
+                    //    priorityQueue.Enqueue(matrix[i][j], matrix[i][j]);
+                    //}
+                    //else
+                    //{
+                    //    if (matrix[i][j] > priorityQueue.Peek())
+                    //    {
+                    //        priorityQueue.EnqueueDequeue(matrix[i][j], matrix[i][j]);
+                    //    }
+                    //}
+                    //if (fakePriorityQueue.Count < k)
+                    //{
+                    //    fakePriorityQueue.Add(matrix[i][j]);
+                    //    if (fakePriorityQueue.Count == k)
+                    //    {
+                    //        fakePriorityQueue.Sort();
+                    //    }
+                    //}
+                    //else
+                    //{
+                    //    if (matrix[i][j] > fakePriorityQueue.First())
+                    //    {
+                    //        fakePriorityQueue.RemoveAt(0);
+                    //        fakePriorityQueue.Add(matrix[i][j]);
+                    //        fakePriorityQueue.Sort();
+                    //    }
+                    //}
+                    temp[count++] = matrix[i][j];
+                }
+            }
+            //Time: O(MNlogk)
+            //Space: O(k)
+            //return priorityQueue.Peek();
+
+            //Time: O(MNklogk) can be optimized to O(MNk) by rebuilding the fakePriorityQueue
+            //Space: O(k)
+            //return fakePriorityQueue.First();
+
+            //Time: O(MN)
+            //Space: O(MN)
+            System.Array.Sort(temp, (x, y) => y.CompareTo(x));
+            return temp[k - 1];
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using ConsoleApp1.BinaryTree;
+﻿using ConsoleApp1._2dArray;
 
 namespace ConsoleApp1;
 
@@ -6,20 +6,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        var node11 = new TreeNode(11);
-        var node_3 = new TreeNode(-3, right: node11);
-
-        var node3__2 = new TreeNode(3);
-        var node_2 = new TreeNode(-2);
-        var node3__1 = new TreeNode(3, node3__2, node_2);
-
-        var node1 = new TreeNode(1);
-        var node2 = new TreeNode(2, right: node1);
-        var node5 = new TreeNode(5, node3__1, node2);
-
-        var node10 = new TreeNode(1, new TreeNode(-2), new TreeNode(-3));
-
-        var t = new _437().PathSum(node10, -1);
+        var t = new _1738().KthLargestValue(new[] { new[] { 5, 2 }, new[] { 1, 6 } }, 1);
         Console.WriteLine('s');
+
     }
 }
