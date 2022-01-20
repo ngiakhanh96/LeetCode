5079d342516a4c9865ffae57aa2533518a75fa62
ngiakhanh <ngiakhanh96@gmail.com>
2022-01-21 00:53:41 +0700

    Review lesson 6

--- ConsoleApp1/HashTable/_974.cs
+++ ConsoleApp1/HashTable/_974.cs
@@ -30,4 +30,49 @@ public class _974
         }
         return res;
     }
+
+    public int SubarraysDivByK2(int[] nums, int k)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            nums[i] += (i > 0 ? nums[i - 1] : 0);
+        }
+        for (var i = 0; i < nums.Length; i++)
+        {
+            nums[i] %= k;
+        }
+
+        var count = 0;
+        foreach (var num in nums)
+        {
+            if (num % k == 0)
+            {
+                count++;
+            }
+
+            var lookingLeftNum1 = (-k + num) % k;
+            var lookingRightNum2 = (k + num) % k;
+            if (dict.ContainsKey(lookingLeftNum1))
+            {
+                count += dict[lookingLeftNum1];
+            }
+
+            if (lookingLeftNum1 != lookingRightNum2)
+            {
+                if (dict.ContainsKey(lookingRightNum2))
+                {
+                    count += dict[lookingRightNum2];
+                }
+            }
+
+
+            if (!dict.TryAdd(num, 1))
+            {
+                dict[num]++;
+            }
+        }
+        return count;
+    }
 }
