55dbbcd0682856fc33c0c9ece52faf6229780f7b
WhiteK <khanh96.ng@gmail.com>
2023-07-26 01:09:58 +0800

    Add matrix

---
+++ ConsoleApp1/Matrix/_48.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1.Matrix;
+
+[LastVisited(2023, 07, 25)]
+public class _48
+{
+    public void Rotate(int[][] matrix)
+    {
+        var numLayers = (Math.Min(matrix.Length, matrix[0].Length) + 1) / 2;
+        for (var layer = 0; layer < numLayers; layer++)
+        {
+            var currentRow = layer;
+            for (var col = layer; col < matrix[0].Length - 1 - layer; col++)
+            {
+                var currentCol = col;
+                var temp = matrix[currentRow][currentCol];
+                for (var y = 0; y < 4; y++)
+                {
+                    var newCol = matrix.Length - 1 - currentRow;
+                    var newRow = currentCol;
+                    (matrix[newRow][newCol], temp) = (temp, matrix[newRow][newCol]);
+                    currentCol = newCol;
+                    currentRow = newRow;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Matrix/_54.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleApp1.Matrix;
+
+[LastVisited(2023, 07, 25)]
+public class _54
+{
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        var result = new List<int>();
+        var numLayers = (Math.Min(matrix.Length, matrix[0].Length) + 1) / 2;
+        for (var layer = 0; layer < numLayers; layer++)
+        {
+            var currentCol = layer;
+            var currentRow = layer;
+            while (currentCol < matrix[0].Length - layer)
+            {
+                result.Add(matrix[currentRow][currentCol]);
+                currentCol++;
+            }
+            currentCol--;
+            if (matrix.Length == currentRow * 2 + 1)
+            {
+                break;
+            }
+
+            currentRow++;
+            while (currentRow < matrix.Length - layer)
+            {
+                result.Add(matrix[currentRow][currentCol]);
+                currentRow++;
+            }
+            currentRow--;
+            if (matrix[0].Length == currentCol * 2 + 1)
+            {
+                break;
+            }
+
+            currentCol--;
+            while (currentCol >= layer)
+            {
+                result.Add(matrix[currentRow][currentCol]);
+                currentCol--;
+            }
+            currentCol++;
+
+            currentRow--;
+            while (currentRow > layer)
+            {
+                result.Add(matrix[currentRow][currentCol]);
+                currentRow--;
+            }
+        }
+        return result;
+    }
+}
