38159f979102ea9f1075a29ebcf216c58513ad46
WhiteK <khanh96.ng@gmail.com>
2023-07-27 00:48:32 +0800

    Add matrix and dfs

---
+++ ConsoleApp1/DFS/_79.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp1.DFS;
+
+public class _79
+{
+    public bool Exist(char[][] board, string word)
+    {
+        var characters = word.ToCharArray();
+        var visited = new bool[board.Length, board[0].Length];
+        const int pointer = 0;
+        for (var row = 0; row < board.Length; row++)
+        {
+            for (var col = 0; col < board[0].Length; col++)
+            {
+                if (board[row][col] == characters[0] && !visited[row, col])
+                {
+                    if (Dfs(row, col, board, characters, visited, pointer))
+                    {
+                        return true;
+                    }
+                    visited[row, col] = false;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static bool Dfs(int row, int col, IReadOnlyList<char[]> board, IReadOnlyList<char> characters, bool[,] visited, int pointer)
+    {
+        if (pointer + 1 == characters.Count)
+        {
+            return true;
+        }
+        visited[row, col] = true;
+        var nextRows = new[] {
+            new[]{row - 1, col},
+            new[]{row + 1, col},
+            new[]{row, col - 1},
+            new[]{row, col + 1}
+        };
+
+        foreach (var nextRow in nextRows)
+        {
+            var nextRowX = nextRow[0];
+            var nextRowY = nextRow[1];
+            if (nextRowX >= 0 && nextRowX < board.Count && nextRowY >= 0 && nextRowY < board[0].Length &&
+                board[nextRowX][nextRowY] == characters[pointer + 1] &&
+                !visited[nextRowX, nextRowY])
+            {
+                if (Dfs(nextRowX, nextRowY, board, characters, visited, pointer + 1))
+                {
+                    return true;
+                }
+                visited[nextRowX, nextRowY] = false;
+            }
+        }
+        return false;
+    }
+}
---
+++ ConsoleApp1/Matrix/_73.cs
@@ -0,0 +1,72 @@
+﻿namespace ConsoleApp1.Matrix;
+
+public class _73
+{
+    public void SetZeroes(int[][] matrix)
+    {
+        var shouldSetFirstRowToZero = false;
+        var shouldSetFirstColToZero = false;
+        for (var row = 0; row < matrix.Length; row++)
+        {
+            for (var col = 0; col < matrix[0].Length; col++)
+            {
+                if (matrix[row][col] == 0)
+                {
+                    if (row == 0)
+                    {
+                        shouldSetFirstRowToZero = true;
+                    }
+                    else
+                    {
+                        matrix[row][0] = 0;
+                    }
+
+                    if (col == 0)
+                    {
+                        shouldSetFirstColToZero = true;
+                    }
+                    else
+                    {
+                        matrix[0][col] = 0;
+                    }
+                }
+            }
+        }
+        for (var col = 1; col < matrix[0].Length; col++)
+        {
+            if (matrix[0][col] == 0)
+            {
+                for (var row = 0; row < matrix.Length; row++)
+                {
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+        for (var row = 1; row < matrix.Length; row++)
+        {
+            if (matrix[row][0] == 0)
+            {
+                for (var col = 0; col < matrix[0].Length; col++)
+                {
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+
+        if (shouldSetFirstColToZero)
+        {
+            for (var row = 0; row < matrix.Length; row++)
+            {
+                matrix[row][0] = 0;
+            }
+        }
+
+        if (shouldSetFirstRowToZero)
+        {
+            for (var col = 0; col < matrix[0].Length; col++)
+            {
+                matrix[0][col] = 0;
+            }
+        }
+    }
+}
