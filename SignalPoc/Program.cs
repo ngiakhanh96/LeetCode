68529c91481ac6d9f5ac100b30166324c1cb4467
WhiteK <khanh96.ng@gmail.com>
2024-02-26 00:11:49 +0800

    Add signal

---
+++ SignalPoc/Program.cs
@@ -0,0 +1,52 @@
+﻿using SignalPoc;
+
+Console.WriteLine("Hello, World!");
+
+var a = SignalOop.CreateSignal(1);
+Console.WriteLine("a is " + a.Get());
+var b = SignalOop.CreateSignal(2);
+Console.WriteLine("b is " + b.Get());
+var effect = SignalOop.CreateEffect(() =>
+{
+    Console.WriteLine("Sum is " + (a.Get() + b.Get()));
+});
+
+a.Update(val => val + 1); //2
+
+b.Update(val => val + 1); //3
+
+var (c, subscriptionC) = SignalOop.ComputeSignal(() => a.Get() + b.Get());
+Console.WriteLine("c is " + c.Get());
+a.Update((val) => val + 1);
+Console.WriteLine("a is " + a.Get());
+b.Update((val) => val + 1);
+Console.WriteLine("b is " + b.Get());
+Console.WriteLine("c is " + c.Get());
+
+var (d, subscriptionD) = SignalOop.ComputeSignal(() => c.Get() * 2);
+Console.WriteLine("d is " + d.Get());
+
+c.Update((val) => val + 1);
+Console.WriteLine("a is " + a.Get());
+Console.WriteLine("b is " + b.Get());
+Console.WriteLine("c is " + c.Get());
+Console.WriteLine("d is " + d.Get());
+
+Console.WriteLine("Unsubscribe effect and c");
+effect.Unsubscribe();
+subscriptionC.Unsubscribe();
+a.Update(val => val + 1);
+b.Update(val => val + 1);
+Console.WriteLine("a is " + a.Get());
+Console.WriteLine("b is " + b.Get());
+Console.WriteLine("No sum shown");
+Console.WriteLine("c is " + c.Get());
+Console.WriteLine("d is " + d.Get());
+
+Console.WriteLine("Unsubscribe d");
+subscriptionD.Unsubscribe();
+c.Update(val => val + 1);
+Console.WriteLine("c is " + c.Get());
+Console.WriteLine("d is " + d.Get());
+
+Console.ReadKey();
---
+++ SignalPoc/Signal.cs
@@ -0,0 +1,76 @@
+﻿namespace SignalPoc;
+
+internal static class SignalOop
+{
+    public static Action? CurrentSubscription;
+    public static List<SignalBase> CurrentSignals = [];
+    public static SignalSubscription CreateEffect(Action action)
+    {
+        CurrentSubscription = action;
+        action();
+        var currentSignals = new SignalSubscription(CurrentSignals, CurrentSubscription);
+        CurrentSubscription = null;
+        CurrentSignals = [];
+        return currentSignals;
+    }
+
+    public static (Signal<T?> Signal, SignalSubscription Subscription) ComputeSignal<T>(Func<T> action)
+    {
+        Signal<T?> computedSignal = new(default);
+        var subscription = CreateEffect(() =>
+        {
+            var value = action();
+            computedSignal.Update(val => value);
+        });
+        return (computedSignal, subscription);
+    }
+
+    public static Signal<T> CreateSignal<T>(T value)
+    {
+        return new Signal<T>(value);
+    }
+}
+
+internal record SignalSubscription(List<SignalBase> Signals, Action Action)
+{
+    public void Unsubscribe()
+    {
+        foreach (var signal in Signals)
+        {
+            signal.RemoveSubscription(Action);
+        }
+    }
+}
+
+internal abstract class SignalBase
+{
+    protected readonly HashSet<Action> Subscriptions = [];
+
+    public void RemoveSubscription(Action action)
+    {
+        Subscriptions.Remove(action);
+    }
+}
+
+internal class Signal<T>(T value) : SignalBase
+{
+    public T Get()
+    {
+        if (SignalOop.CurrentSubscription is not null)
+        {
+            Subscriptions.Add(SignalOop.CurrentSubscription);
+            SignalOop.CurrentSignals.Add(this);
+        }
+
+        return value;
+    }
+
+    public void Update(Func<T, T> action)
+    {
+        value = action(value);
+        foreach (var subscription in Subscriptions)
+        {
+            subscription();
+        }
+    }
+}
